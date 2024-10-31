c416c56d261b236766156d52ac7e57cb0b22f663
AmerShammout <amershammout2004@gmail.com>
2024-10-31 23:49:37 +0300

    setup-project

---
+++ lib/utils/app_icons.dart
@@ -0,0 +1,101 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for iconsDocFile
+  /// assets/icons/doc_file.svg
+  static const String iconsDocFile = "assets/icons/doc_file.svg";
+
+  /// Assets for iconsDocuments
+  /// assets/icons/Documents.svg
+  static const String iconsDocuments = "assets/icons/Documents.svg";
+
+  /// Assets for iconsDropBox
+  /// assets/icons/drop_box.svg
+  static const String iconsDropBox = "assets/icons/drop_box.svg";
+
+  /// Assets for iconsExcleFile
+  /// assets/icons/excle_file.svg
+  static const String iconsExcleFile = "assets/icons/excle_file.svg";
+
+  /// Assets for iconsFigmaFile
+  /// assets/icons/Figma_file.svg
+  static const String iconsFigmaFile = "assets/icons/Figma_file.svg";
+
+  /// Assets for iconsFolder
+  /// assets/icons/folder.svg
+  static const String iconsFolder = "assets/icons/folder.svg";
+
+  /// Assets for iconsGoogleDrive
+  /// assets/icons/google_drive.svg
+  static const String iconsGoogleDrive = "assets/icons/google_drive.svg";
+
+  /// Assets for iconsLogo
+  /// assets/icons/logo.svg
+  static const String iconsLogo = "assets/icons/logo.svg";
+
+  /// Assets for iconsMedia
+  /// assets/icons/media.svg
+  static const String iconsMedia = "assets/icons/media.svg";
+
+  /// Assets for iconsMediaFile
+  /// assets/icons/media_file.svg
+  static const String iconsMediaFile = "assets/icons/media_file.svg";
+
+  /// Assets for iconsMenuDashbord
+  /// assets/icons/menu_dashbord.svg
+  static const String iconsMenuDashbord = "assets/icons/menu_dashbord.svg";
+
+  /// Assets for iconsMenuDoc
+  /// assets/icons/menu_doc.svg
+  static const String iconsMenuDoc = "assets/icons/menu_doc.svg";
+
+  /// Assets for iconsMenuNotification
+  /// assets/icons/menu_notification.svg
+  static const String iconsMenuNotification = "assets/icons/menu_notification.svg";
+
+  /// Assets for iconsMenuProfile
+  /// assets/icons/menu_profile.svg
+  static const String iconsMenuProfile = "assets/icons/menu_profile.svg";
+
+  /// Assets for iconsMenuSetting
+  /// assets/icons/menu_setting.svg
+  static const String iconsMenuSetting = "assets/icons/menu_setting.svg";
+
+  /// Assets for iconsMenuStore
+  /// assets/icons/menu_store.svg
+  static const String iconsMenuStore = "assets/icons/menu_store.svg";
+
+  /// Assets for iconsMenuTask
+  /// assets/icons/menu_task.svg
+  static const String iconsMenuTask = "assets/icons/menu_task.svg";
+
+  /// Assets for iconsMenuTran
+  /// assets/icons/menu_tran.svg
+  static const String iconsMenuTran = "assets/icons/menu_tran.svg";
+
+  /// Assets for iconsOneDrive
+  /// assets/icons/one_drive.svg
+  static const String iconsOneDrive = "assets/icons/one_drive.svg";
+
+  /// Assets for iconsPdfFile
+  /// assets/icons/pdf_file.svg
+  static const String iconsPdfFile = "assets/icons/pdf_file.svg";
+
+  /// Assets for iconsSearch
+  /// assets/icons/Search.svg
+  static const String iconsSearch = "assets/icons/Search.svg";
+
+  /// Assets for iconsSoundFile
+  /// assets/icons/sound_file.svg
+  static const String iconsSoundFile = "assets/icons/sound_file.svg";
+
+  /// Assets for iconsUnknown
+  /// assets/icons/unknown.svg
+  static const String iconsUnknown = "assets/icons/unknown.svg";
+
+  /// Assets for iconsXdFile
+  /// assets/icons/xd_file.svg
+  static const String iconsXdFile = "assets/icons/xd_file.svg";
+}
+
